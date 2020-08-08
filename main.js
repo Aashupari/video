043e5a79ebffe53b6c9e06c1153b3174df9209ce
Aashupari <68390226+Aashupari@users.noreply.github.com>
2020-08-08 18:55:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
+video="";
+status="";
+
 function setup()
 {
     canvas=createCanvas(500, 400);
     canvas.center();
+}
+
+function preload()
+{
+    video= createVideo('video.mp4');
+    video.hide();
+}
+
+function draw()
+{
+    image(video, 0, 0, 500, 400);
+}
+
+function start()
+{
+    objectDetector= ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML="Status: detecting object";
+}
+
+function modelLoaded()
+{
+    console.log("Model is loaded")
+    video.loop();
+    video.speed(1);
+    video.volume(1);
+    status=true;
 }
