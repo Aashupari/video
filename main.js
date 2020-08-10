c22e382ba425794cdcb40548a99aaaab376ecacc
Aashupari <68390226+Aashupari@users.noreply.github.com>
2020-08-10 18:03:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 video="";
 status="";
+objects=[];
 
 function setup()
 {
@@ -16,6 +17,21 @@ function preload()
 function draw()
 {
     image(video, 0, 0, 500, 400);
+    if(status!=""){
+        objectDetector.detect(video, gotResult);
+
+        for(i=0; i<objects.length; i++)
+        {
+            document.getElementById("status").innerHTML="status: object detected";
+            document.getElementById("no_of_objects").innerHTML="No of objects are"+objects.length;
+            fill('#eb3434');
+            stroke('#eb3434');
+            noFill();
+            percent=floor(objects[i].confidence*100);
+            text(objects[i].label + " " + percent + "%",objects[i].x + 15, objects[i].y + 15);
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+        }
+    }
 }
 
 function start()
@@ -31,4 +47,16 @@ function modelLoaded()
     video.speed(1);
     video.volume(1);
     status=true;
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        objects=results;
+    }
 }
